de99c225e702d0fb6271878d0bbce6c7ba462d1f
maulik231 <maulikchodvadiya231@gmail.com>
2022-10-18 00:18:32 +0530

    update code and added readme file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # node_typescript
+
+Install the dependencies:
+
+```bash
+$ npm install
+```
+
+Use the command below to start the API server:
+
+```bash
+$ npm run dev
+```
--- controller/users.controller.ts
+++ controller/users.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import db from "./../models/.";
+import db from "./../models/index";
 
 async function getUsers(req: Request, res: Response) {
   try {
